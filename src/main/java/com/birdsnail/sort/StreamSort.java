ac55bdad3660caa2499e7fce7ff90e77ec369e5c
BirdSnail <314726028@qq.com>
2022-10-10 17:42:08 +0800

    Initial commit

---
+++ src/main/java/com/birdsnail/sort/StreamSort.java
@@ -0,0 +1,24 @@
+package com.birdsnail.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamSort {
+
+    // 可以正确的排序
+    public static List<Human> sort1(List<Human> humans) {
+        return humans.stream()
+                .sorted(Comparator.comparing(Human::getName).reversed())
+                .collect(Collectors.toList());
+    }
+
+    // 编译失败，泛型检测失败
+    public static List<Human> sort2(List<Human> humans) {
+        return humans.stream()
+                .sorted(Comparator.comparing(it -> it.getName()).reversed())
+                .collect(Collectors.toList());
+    }
+
+}
